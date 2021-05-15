6c17226c1073a605b86053a9ac91d818fa1556dc
Anna Sousa <anna.chucky@hotmail.com>
2021-05-15 14:30:34 -0300

    Inserido visualizar senha

--- js/cadastro.js
+++ js/cadastro.js
@@ -14,3 +14,41 @@ $('#cep').focusout(function(){
 
 });
 
+$('#olho').mouseup(function(){
+
+    $('#password').attr("type", "text");
+
+});
+
+$('#olho').mouseout(function(){
+
+    $('#password').attr("type", "password");
+});
+
+$('#olho2').mouseup(function(){
+
+    $('#password2').attr("type", "text");
+
+});
+
+$('#olho2').mouseout(function(){
+
+    $('#password2').attr("type", "password");
+});
+
+$('#email').focusout(function(){
+
+    let email = $('#email').val()
+    let arroba = 0;
+
+    for (let i = 0; i < email.length; i++ ){
+        
+        if(email[i] == '@' || email[i] == '.' ) {
+            arroba++
+        }
+    }
+    console.log(arroba)
+  //  if(arroba==1){
+
+   // }
+})
